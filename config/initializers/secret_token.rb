a3b535a9d7b3c267d2682c7c2ba9c3b33bcf1f09
Brad Carter <bcarter@thebcblends.com>
2011-09-10 12:55:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KernDist::Application.config.secret_token = '8a8cd94cdd93d093d4cc9421b7f82e5cc742cde79bc988797018589ade20450142fc99d30150133345c5a9f113b428883ce6d5ed684e7a2b7cfd58f6d056b395'
